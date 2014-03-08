234c94c7767c82a3f7482312b30689afc4714110
jose <jose@eyeos.com>
2014-03-08 19:17:44 +0100

    balls

--- PoCs/graphic/test.js
+++ PoCs/graphic/test.js
@@ -1,11 +1,14 @@
 var cnv = null;
 var context = null;
+var h,w;
 
-var streamProps = {
-	bps: 0,
-	volume: 0,
-	note: 0
-}
+var props = {
+	particles: 100
+};
+
+len = props.particles;
+
+var model = [];
 
 $( document ).ready(function() {
 	cnv = $('#display')[0];
@@ -22,27 +25,61 @@ function fullScreenCanvas() {
 	"use strict";
 	cnv.width = $(document).width();
 	cnv.height = $(document).height();
+	h = cnv.height;
+	w = cnv.width;
 }
 
 function init() {
 	"use strict";
 
-	var color = 0;
-	var direction = 1;
+	for(var i=0; i<props.particles; i++) {
+		var entity = {
+			x: Math.floor(Math.random() * w) + 1,
+			y: Math.floor(Math.random() * h) + 1,
+			speedY: Math.floor(Math.random() * 20) + 1,
+			speedX: Math.floor(Math.random() * 20) + 1
+		};
 
+		model.push(entity);
+	}
 
 	setInterval(function() {
-		//graphical code here
-		//updateStreamProps();
-		
+		loop();
 	}, 16);
 }
 
-function updateStreamProps() {
+function loop() {
 	"use strict";
-	streamProps = {
-		bps: 20,
-		volume: 20,
-		note:40
-	};
+	//draw model
+	context.clearRect ( 0 , 0 , w, h );
+	context.fillStyle = 'white';
+	var len = model.length;
+
+	var entity;
+	while(len--) {
+		entity = model[len];
+
+		//draw a circle
+		context.beginPath();
+		context.arc(entity.x, entity.y, 2, 0, Math.PI*2, true);
+		context.closePath();
+		context.fill();
+
+		entity.x += entity.speedX;
+		entity.y += entity.speedY;
+
+		if(entity.x < 0 || entity.x > w) {
+			entity.speedX = entity.speedX*-1;
+		}
+
+		if(entity.y < 0 || entity.y > h) {
+			entity.speedY = entity.speedY*-1;
+		}
+	}
+
+	//
+	len = model.length;
+	while(len--) {
+		
+	}
 }
