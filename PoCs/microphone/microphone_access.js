dcf0bea669c43e9ee985be8311facffef0cd215b
FerCa <ferca23@gmail.com>
2014-03-11 00:50:35 +0100

    Handle beat analysis using scriptprocessor onaudioprocess event instead of using a setinterval

--- PoCs/microphone/microphone_access.js
+++ PoCs/microphone/microphone_access.js
@@ -14,50 +14,40 @@ var errorCallback = function errorCallbackFunction(error) {
     console.log(error);
 };
 
-var first = 0;
-var audioProcessCallback = function audioProcessCallback(e) {
-    var buffer = e.inputBuffer.getChannelData(0);
-
-    if (first<100) {
-        console.log(e);
-        console.log('Buffer: ', buffer);
-        first++;
-    }
-}
+var bd = new BeatDetektor();
 
 var gotStreamCallback = function gotStreamCallbackFunction(mediaStream) {
     var microphoneSource = audioContext.createMediaStreamSource(mediaStream);
 
     // Add a delay node to create a delay effect
     var delayNode = audioContext.createDelay(100);
-    delayNode.delayTime.value = 2;
+    delayNode.delayTime.value = 0;
     microphoneSource.connect(delayNode);
 
     // Add a analyser node to be able to get data about the signal
     var analyserNode = audioContext.createAnalyser();
-    var frequencyData = new Uint8Array(analyserNode.frequencyBinCount);
+   // var frequencyData = new Uint8Array(analyserNode.frequencyBinCount);
     delayNode.connect(analyserNode);
 
-    // To get the stream in a buffer
-    //var audioNode = audioContext.createJavaScriptNode(2048, 1, 1);
-    //audioNode.onaudioprocess = audioProcessCallback;
-    //analyserNode.connect(audioNode);
+    var javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+    analyserNode.connect(javascriptNode);
+
+    javascriptNode.onaudioprocess = function(e) {
+        var inputArrayL = e.inputBuffer.getChannelData(0);
+
+        var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
+        analyserNode.getByteFrequencyData(freqByteData);
+
+        console.log(audioContext.currentTime);
+
+        bd.process(audioContext.currentTime, freqByteData);
+
+        console.log((bd.win_bpm_int/10.0)+" BPM / "+(bd.win_bpm_int_lo)+" BPM");
+
+    }
 
     // Connect to the sound output
-    analyserNode.connect(audioContext.destination);
-
-    // Interval to get the frequency data every second
-    bd_low = new BeatDetektor(48,95);
-    var countSeconds = 0;
-    var interval = setInterval(function() {
-        analyserNode.getByteFrequencyData(frequencyData);
-        console.log('Frequency Data: ', frequencyData);
-        bd_low.process(countSeconds, frequencyData);
-        console.log('BPM: ', bd_low.win_bpm_int_lo);
-
-        countSeconds++;
-        if (countSeconds>60) clearInterval(interval);
-    }, 1000);
+    javascriptNode.connect(audioContext.destination);
 }
 
 navigator.getMedia({audio: true}, gotStreamCallback, errorCallback);
